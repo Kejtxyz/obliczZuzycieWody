edce7894696686a5cb5466747c3f6be8f509cd6e
Kasia <kasiag@gmx.com>
2020-05-05 04:19:45 +0200

    tst jednostkowy pierwszy - tworzy plik - sprawdza czy plik zostal utowrzony -assercija false, kiedy jest null

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.PrintWriter;
@@ -7,8 +8,20 @@ public class test1 {
    @Test
 public void sprawdzZapisaniePliku() throws Exception{
        PrintWriter zapis = new PrintWriter("test.txt");
-       zapis.println("test.txt");
-    //   Assert.assertTrue((new PrintWriter("text.txt"));
-       System.out.println("plik zostal zapisany ,test zapisal plik i sprawdzil ,przeszedl pozytywnie");
+       zapis.println("aaaa");
+       Assert.assertFalse(zapis.equals(null));
+       System.out.println("TEST - plik zostal utworzony ,sprawdzil ,przeszedl pozytywnie");
    }
+//@Test
+    //    public void zapiszcosdopliku() throws Exception {
+    // zapisz cos do pliku testowego
+  //  zapis.println("aaaa");
+//    Assert.assertTrue(zapis.equals("aaaa"));
+
+  //  Assert.assertTrue(zapis.equals("aaaa"));
+//}
+    //usun utworzony plik do testow
+
+
+
 }
